f200d2026fddde2f114afda17aa2cf6686fb0ebd
Zhile Lin <zhlin04@gmail.com>
2023-05-02 12:46:02 -0700

    native card

--- src/components/modals/ReactNativeModal.jsx
+++ src/components/modals/ReactNativeModal.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 const ReactNativeModal = () => {
   return (
-    <div>
-        
+    <div className='mt-[20px] text-[20px] flex flex-col items-center'>
+        During the job hunt I built a mobile app that assist me in job hunting process, It starts with curiosity but I found it quite fun to transfer what I know into mobile app developement. 
+        <br />
+        <br />
+        <br />
+        <br />
+        <div className='text-[18px] p-[10px] '>I am planning to start another one soon . . . stay tune!</div>
     </div>
   )
 }
