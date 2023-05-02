6a5952eba7f66cdf7006d52b93f1e0965b5dfb13
Zhile Lin <zhlin04@gmail.com>
2023-05-02 13:35:36 -0700

    cofounder card

--- src/components/modals/BackendModal.jsx
+++ src/components/modals/BackendModal.jsx
@@ -2,9 +2,17 @@ import React from 'react'
 
 const BackendModal = () => {
   return (
-    <div>
-      
+    <div className='text-[20px] flex flex-col items-center'>
+       Backend to me is the bread and butter of web development. I seek to improve my backend skill by working on building the logic, architecture, and functionality of the web application in a daily basis. 
+       <br />
+       <br />
+       <br />
+       <div className='text-[17px] text-regular text-slate-300'>
+           Currently I am working on drawing out a database diagram for my startup project. Leave me some tips if you know how to do it !
+       </div>
     </div>
+   
+
   )
 }
 
--- src/components/modals/CoFounderModal.jsx
+++ src/components/modals/CoFounderModal.jsx
@@ -2,9 +2,17 @@ import React from 'react'
 
 const CoFounderModal = () => {
   return (
-    <div>
-      Hi
+    <div className='text-[20px] mt-[15px] flex flex-col items-center'>
+        Working with a team of brilliant scientists and software developers on building a platform where we had a vision on long term market growth. I've learnt a lot from the project and proud to be a part of it. 
+        <br />
+        <br />
+        <br />
+        <br />
+        <div className='text-[17px]'>
+          check out our platform <a href="https://astorscientific.com/" target="_blank" className='text-violet-400'>astorscientific.com</a>
+        </div>
     </div>
+    
   )
 }
 
